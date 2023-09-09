cbb249635b667d3a2ca5dbf0f55c6d91402bcc12
Gowthaman2008 <110714953+Gowthaman2008@users.noreply.github.com>
2023-09-09 17:09:14 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -634,17 +634,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InputMediaPhoto(random.choice(PICS), script.SOURCE_TXT, enums.ParseMode.HTML),
             reply_markup=reply_markup,   
         )
-      elif query.data == "CR":
-        buttons = [[
-            InlineKeyboardButton(' CR_Linkzz ', url='https://t.me/CR_Tamil_Movies_Official')
-            ],[
-            InlineKeyboardButton('👩‍🦯 𝙱𝙰𝙲𝙺', callback_data='about'),
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.edit_message_media(
-            InputMediaPhoto(random.choice(PICS), script.CR_TXT, enums.ParseMode.HTML),
-            reply_markup=reply_markup,
-        )
     elif query.data == "restric":
         buttons = [[
             InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='help')
